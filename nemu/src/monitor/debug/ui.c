b0ddd5256ea0ebe8f36fa5ba17563a6879d34409
tracer-ics2015 <tracer@njuics.org>
2015-09-25 18:29:30 +0800

    >  compile NEMU 14307130167 zyr17 Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  18:29:30 up 1 day, 16:58,  2 users,  load average: 0.28, 0.23, 0.18 a33e3968d0ccd4cc8e83c7fe738b944349aa2f15

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -39,6 +39,7 @@ static int cmd_q(char *args) {
 static int cmd_si(char *args) {
     int times = 1, len = strlen(args);
     sscanf(args, "%d", &times);
+    printf("%d %s\n", len, args);
     if (times < 1 || len == 0){
         printf("Wrong input\n");
         return 0;
