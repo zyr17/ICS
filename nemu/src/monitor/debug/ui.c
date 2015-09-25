1a405c2b1fa8b89acd35141fa0dcf99b2a705924
tracer-ics2015 <tracer@njuics.org>
2015-09-25 18:33:14 +0800

    >  compile NEMU 14307130167 zyr17 Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  18:33:14 up 1 day, 17:02,  2 users,  load average: 0.11, 0.17, 0.16 8e2420b26b71a35e28b51499f49d3a74e5da379f

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -40,7 +40,7 @@ static int cmd_si(char *args) {
     //printf("%s\n", args);
     if (args == NULL) return 0;
     int times = 1, len = strlen(args);
-    //printf("%d %s\n", len, args);
+    printf("%d %s\n", len, args);
     sscanf(args, "%d", &times);
     return 0;
     if (times < 1 || len == 0){
