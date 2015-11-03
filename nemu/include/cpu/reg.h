a77a4c3968c11988fe0bb6ce1fe0fd4f0074a4e7
tracer-ics2015 <tracer@njuics.org>
2015-11-04 00:37:09 +0800

    >  compile NEMU 14307130167 zyr17 Linux ubuntu 3.19.0-28-generic #30~14.04.1-Ubuntu SMP Tue Sep 1 09:32:55 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  00:37:09 up 23:44,  2 users,  load average: 0.20, 0.16, 0.14 276e8b6d90cefa47834b09774593a3f08e9dd6d6

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -30,6 +30,29 @@ typedef struct {
 	};
 
 	swaddr_t eip;
+    union{
+        uint32_t eflags;
+        struct{
+            unsigned CF:1;
+            unsigned :1;
+            unsigned PF:1;
+            unsigned :1;
+            unsigned AF:1;
+            unsigned :1;
+            unsigned ZF:1;
+            unsigned SF:1;
+            unsigned TF:1;
+            unsigned IF:1;
+            unsigned DF:1;
+            unsigned OF:1;
+            unsigned IOPL:2;
+            unsigned NT:1;
+            unsigned :1;
+            unsigned RF:1;
+            unsigned VM:1;
+        };
+    };
+
 
 } CPU_state;
 
