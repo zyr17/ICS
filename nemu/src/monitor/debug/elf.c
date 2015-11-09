d174c82bc69c5258efd9fb8915fe12b0c28a0271
tracer-ics2015 <tracer@njuics.org>
2015-11-09 20:29:44 +0800

    >  compile NEMU 14307130167 zyr17 Linux ubuntu 3.19.0-28-generic #30~14.04.1-Ubuntu SMP Tue Sep 1 09:32:55 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  20:29:44 up 1 day, 19:02,  3 users,  load average: 0.42, 0.29, 0.20 4682cb8c696fa520efb6b8982d045874dd6037e7

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -107,7 +107,7 @@ void bt_print(int now, uint32_t ebp, uint32_t eip){
         //for (; i < nr_symtab_entry; i ++ )
         //    if (symtab[i].st_value <= eip && symtab[i].st_value + symtab[i].st_size >= eip)
         //        printf("#%03d 0x%x in %s\n", now, eip, strtab + symtab[i].st_name);
-        Log("#%03d 0x%x in %s\n", now, eip, strtab + symtab[i].st_name);
+        //Log("#%03d 0x%x in %s\n", now, eip, strtab + symtab[i].st_name);
         return;
     }
     for (; i < nr_symtab_entry; i ++ )
