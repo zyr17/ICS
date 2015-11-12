1ba8eb9f057cd39a941e243537e66f56318aad6e
tracer-ics2015 <tracer@njuics.org>
2015-11-12 12:24:00 +0800

    >  compile NEMU 14307130167 zyr17 Linux ubuntu 3.19.0-28-generic #30~14.04.1-Ubuntu SMP Tue Sep 1 09:32:55 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  12:24:00 up  2:03,  2 users,  load average: 0.45, 0.25, 0.23 bc90ee25dd93c63540a380b15f4e576cb5bd0e87

---
+++ nemu/src/cpu/exec/data-mov/movzx-template.h
@@ -0,0 +1,25 @@
+#include "cpu/exec/template-start.h"
+
+#define instr movzx_b
+
+static void do_execute() {
+    DATA_TYPE tv = (uint8_t)op_src -> val;
+
+	OPERAND_W(op_dest, tv);
+	print_asm("moxzx" str(SUFFIX) " %s,%s", op_src->str, op_dest->str);
+}
+
+make_instr_helper(rm2r)
+
+#if DATA_BYTE == 4
+make_helper(movzx_w_rm2r_l) {
+    int len = decode_rm2r_l(eip + 1);
+    DATA_TYPE tv = (uint16_t)op_src -> val;
+
+	OPERAND_W(op_dest, tv);
+	print_asm("moxzx" str(SUFFIX) " %s,%s", op_src->str, op_dest->str);
+	return len + 1;
+}
+#endif
+
+#include "cpu/exec/template-end.h"
