c0e8b87a0689f40fe018b1079fb0daafe6d2b01a
tracer-ics2015 <tracer@njuics.org>
2015-11-13 16:23:49 +0800

    >  compile NEMU 14307130167 zyr17 Linux ubuntu 3.19.0-28-generic #30~14.04.1-Ubuntu SMP Tue Sep 1 09:32:55 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  16:23:49 up  6:19,  2 users,  load average: 0.57, 0.32, 0.27 70c960c193090ad8312955d0a24a404928ee037e

--- nemu/src/cpu/exec/data-mov/cxtx-template.h
+++ nemu/src/cpu/exec/data-mov/cxtx-template.h
@@ -3,11 +3,13 @@
 make_helper(cxtx_0_w){
     if (reg_w(0) < 0) reg_w(2) = 0xffff;
     else reg_w(2) = 0;
+    print_asm("cwtl");
     return 1;
 }
 
 make_helper(cxtx_0_l){
     if (cpu.eax < 0) cpu.edx = 0xffffffff;
     else cpu.edx = 0;
+    print_asm("cltd");
     return 1;
 }
