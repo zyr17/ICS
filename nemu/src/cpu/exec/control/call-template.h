a041ce742f61fc36885159b9977c7c3da654a1b7
tracer-ics2015 <tracer@njuics.org>
2015-11-14 09:39:06 +0800

    >  compile NEMU 14307130167 zyr17 Linux ubuntu 3.19.0-28-generic #30~14.04.1-Ubuntu SMP Tue Sep 1 09:32:55 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  09:39:06 up 19:38,  2 users,  load average: 0.36, 0.30, 0.22 68d074c8c8b763a4842fef8d796525efd558389

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -4,11 +4,17 @@
 
 static void do_execute () {
     push_push(DATA_BYTE, cpu.eip + DATA_BYTE + 1);
-    cpu.eip += op_src->val;
+
+    if (swaddr_read(cpu.eip, 1) == 0xe8) cpu.eip += op_src -> val;
+    else{
+        cpu.EIP_CHANGEABLE = 0;
+        cpu.eip = op_src -> val;
+    }
 
 	print_asm_template1();
 }
 
 make_instr_helper(i)
+make_instr_helper(rm)
 
 #include "cpu/exec/template-end.h"
