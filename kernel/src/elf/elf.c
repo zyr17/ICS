43ab6e83bb8be47f39a93fe9bd69989fcf0e0a9b
tracer-ics2015 <tracer@njuics.org>
2015-11-15 18:33:18 +0800

    >  compile kernel 14307130167 zyr17 Linux ubuntu 3.19.0-28-generic #30~14.04.1-Ubuntu SMP Tue Sep 1 09:32:55 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  18:33:18 up  1:29,  2 users,  load average: 0.30, 0.23, 0.23 557e82088254b8dd6cb62b5459f210f46948dc3c

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -31,12 +31,13 @@ uint32_t loader() {
 	elf = (void*)buf;
 
 	/* TODO: fix the magic number with the correct one */
-	const uint32_t elf_magic = 0xBadC0de;
+	const uint32_t elf_magic = 0x464c457f;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
 
 	/* Load each program segment */
-	panic("please implement me");
+	//panic("please implement me");
+	ph = (void*)(buf + elf -> e_shoff);
 	for(; true; ) {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
@@ -44,11 +45,13 @@ uint32_t loader() {
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
+			ramdisk_read((void*)ph -> p_vaddr, ph -> p_offset, ph -> p_filesz);
 			 
 			 
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
+			memset((void*)(ph -> p_vaddr + ph -> p_filesz), 0, ph -> p_memsz - ph -> p_filesz);
 
 
 #ifdef IA32_PAGE
@@ -58,6 +61,7 @@ uint32_t loader() {
 			if(brk < new_brk) { brk = new_brk; }
 #endif
 		}
+		ph += sizeof(Elf32_Phdr);
 	}
 
 	volatile uint32_t entry = elf->e_entry;
