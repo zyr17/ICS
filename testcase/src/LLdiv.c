a3fe3c169c5462fbc46b5a7e78b4b28693704e50
tracer-ics2015 <tracer@njuics.org>
2015-11-18 14:44:40 +0800

    >  run 14307130167 zyr17 Linux ubuntu 3.19.0-28-generic #30~14.04.1-Ubuntu SMP Tue Sep 1 09:32:55 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  14:44:40 up  2:12,  2 users,  load average: 0.05, 0.05, 0.05 11447f9e926310a86a40f5d3e4edf0f871ff450a

--- testcase/src/LLdiv.c
+++ testcase/src/LLdiv.c
@@ -344,11 +344,11 @@ int naive_memcmp(void *p1, void *p2, int n)
 int main()
 {
     //0x0, 0x7ffffffffffffffe, /* 0x7ffffffffffffffe div 0xffffffffffffffce */
-	long long a = 0x7ffffffffffffffe, b = 0xffffffffffffffce, c = 0, d = 0, e = 0, f = 0x7ffffffffffffffe;
+	/*long long a = 0x7ffffffffffffffe, b = 0xffffffffffffffce, c = 0, d = 0, e = 0, f = 0x7ffffffffffffffe;
 	LLdivLL(a, b, &c, &d);
 	nemu_assert(c == e);
 	nemu_assert(d == f);
-	HIT_GOOD_TRAP;
+	HIT_GOOD_TRAP;*/
     test_LL();
     test_ULL();
     /*int i;
