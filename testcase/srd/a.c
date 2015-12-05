51a380ac08b4619f6fca018773e94f72153b8aeb
tracer-ics2015 <tracer@njuics.org>
2015-12-05 16:31:51 +0800

    >  run 14307130167 zyr17 Linux ubuntu 3.19.0-28-generic #30~14.04.1-Ubuntu SMP Tue Sep 1 09:32:55 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  16:31:51 up  9:02,  2 users,  load average: 0.49, 0.42, 0.22 d7284766dfb31de34310a73628396a65bbeabae

---
+++ testcase/srd/a.c
@@ -0,0 +1,11 @@
+#include "trap.h"
+char str[] = "123456789adsgfbre";
+int func(int k){
+	if (!k) return 0;
+	return func(k - 1) + 1;
+}
+int main(){
+	if (func(100) == 100) HIT_GOOD_TRAP;
+	HIT_BAD_TRAP;
+	return 0;
+}
