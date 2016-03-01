9a0875d55d0f5ef85156358dbb99251ffd8e77bc
tracer-ics2015 <tracer@njuics.org>
2016-03-01 23:26:30 +0800

    >  compile NEMU 14307130167 zyr17 Linux ubuntu 3.19.0-28-generic #30~14.04.1-Ubuntu SMP Tue Sep 1 09:32:55 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  23:26:30 up  5:15,  2 users,  load average: 0.56, 0.36, 0.26 221863a1ae85ed65fbbb3a58d04b5d81b8b7505f

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -7,9 +7,12 @@ void dram_write(hwaddr_t, size_t, uint32_t);
 /* Memory accessing interfaces */
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
-    //printf("\n%d %d: ", (int)addr, (int)len);
 	uint32_t read = L1_cache_read(addr, len) & (~0u >> ((4 - len) << 3));
-    //printf("\n%d %d\n", (int)read, (int)dram_read(addr, len) & (~0u >> ((4 - len) << 3)));
+	uint32_t re2 = dram_read(addr, len) & (~0u >> ((4 - len) << 3));
+	if (read != re2){
+        printf("\n%d %d: ", (int)addr, (int)len);
+        printf("%d %d\n", (int)read, (int)re2);
+    }
     return read;
 }
 
