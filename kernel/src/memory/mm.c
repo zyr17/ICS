5b6b9cdbf6f8650001c793e085bc92d1410d94e4
tracer-ics2015 <tracer@njuics.org>
2016-03-01 18:31:15 +0800

    >  compile kernel 14307130167 zyr17 Linux ubuntu 3.19.0-28-generic #30~14.04.1-Ubuntu SMP Tue Sep 1 09:32:55 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  18:31:14 up 20 min,  2 users,  load average: 0.26, 0.48, 0.41 5d0d92b4ec920fc9f17b33d18ea4848d3dcc639f

--- kernel/src/memory/mm.c
+++ kernel/src/memory/mm.c
@@ -27,9 +27,9 @@ void init_mm() {
 	memset(updir, 0, NR_PDE * sizeof(PDE));
 
 	/* create the same mapping above 0xc0000000 as the kernel mapping does */
-	memcpy(&updir[KOFFSET / PT_SIZE], &kpdir[KOFFSET / PT_SIZE], 
+	memcpy(&updir[KOFFSET / PT_SIZE], &kpdir[KOFFSET / PT_SIZE],
 			(PHY_MEM / PT_SIZE) * sizeof(PDE));
 
-	ucr3.val = (uint32_t)va_to_pa((uint32_t)updir) & ~0x3ff;
+	ucr3.val = (uint32_t)va_to_pa((uint32_t)updir) & ~0xfff;
 }
 
--- testcase/srd/matrix-mul.c
+++ testcase/srd/matrix-mul.c
@@ -308,19 +308,20 @@ int ans[N][N] = {{27825, 6208, 14855, -76487, -23589, 13948, 60449, 9292, 5604, 
 int c[N][N];
 
 int main() {
-
-	HIT_GOOD_TRAP;
-
 	int i, j, k;
+	int loop = 0;
 	for(i = 0; i < N; i ++) {
 		for(j = 0; j < N; j ++) {
 			c[i][j] = 0;
 			for(k = 0; k < N; k ++) {
 				c[i][j] += a[i][k] * b[k][j];
+				loop ++;
 			}
 			nemu_assert(c[i][j] == ans[i][j]);
 		}
 	}
+
+	nemu_assert(loop == N * N * N);
 
 	HIT_GOOD_TRAP;
 
