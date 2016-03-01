749fcbfd373d9f6bdac937d81b724d2889d254a0
tracer-ics2015 <tracer@njuics.org>
2016-03-01 22:44:20 +0800

    >  compile NEMU 14307130167 zyr17 Linux ubuntu 3.19.0-28-generic #30~14.04.1-Ubuntu SMP Tue Sep 1 09:32:55 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  22:44:20 up  4:33,  2 users,  load average: 0.44, 0.23, 0.23 f28d6185148dea72658431a3f72fcd68b20c5257

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -34,9 +34,9 @@ uint32_t L1_cache_single(hwaddr_t addr, size_t len){
 uint32_t L1_cache_read(hwaddr_t addr,size_t len){
     if (addr / (BLOCK_SIZE / 8) != (addr + len - 1) / (BLOCK_SIZE / 8)){
         int tmp = (addr + len - 1) % (BLOCK_SIZE / 8) + 1;
-        uint32_t t1 = L1_cache_single(addr, len - tmp);
+        uint32_t t1 = L1_cache_single(addr + (len - tmp), tmp);
         t1 <<= (tmp * 8);
-        t1 += L1_cache_single(addr + (len - tmp), tmp);
+        t1 += L1_cache_single(addr, len - tmp);
         return t1;
     }
     else return L1_cache_single(addr, len);
