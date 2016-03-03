b603aafd95cf1c7479b06c88a620861ab59ee528
tracer-ics2015 <tracer@njuics.org>
2016-03-03 20:25:49 +0800

    >  compile NEMU 14307130167 zyr17 Linux ubuntu 3.19.0-28-generic #30~14.04.1-Ubuntu SMP Tue Sep 1 09:32:55 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  20:25:49 up 1 day, 16:56,  2 users,  load average: 0.34, 0.65, 1.01 bb02ca805ee5948d94a9f00d771f963071186c2e

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -39,14 +39,14 @@ uint32_t L2_cache_single(hwaddr_t addr, size_t len){
         if (l2_cache_block[group][pos].valid_bit && l2_cache_block[group][pos].dirty_bit){
             l2_cache_block[group][pos].dirty_bit = 0;
             hwaddr_t addr_old = (l2_cache_block[group][pos].tag * L2_SET + group) * (BLOCK_SIZE / 8);
-            //int ii;
-            //for (ii = 0; ii < BLOCK_SIZE / 8; ii ++ )
-            //    dram_write(addr_old + ii, 1, l2_cache_block[group][pos].data[ii]);
+            int ii;
+            for (ii = 0; ii < BLOCK_SIZE / 8; ii ++ )
+                dram_write(addr_old + ii, 1, l2_cache_block[group][pos].data[ii]);
             //unsigned long long lltmp = 0;
             //for (ii = BLOCK_SIZE / 8 - 1; i >= 0; i -- )
             //    lltmp = (lltmp << 8LL) + l2_cache_block[group][pos].data[ii];
-            dram_write(addr_old, 4, l2_cache_block[group][pos].data_32_low);
-            dram_write(addr_old + 4, 4, l2_cache_block[group][pos].data_32_high);
+            //dram_write(addr_old, 4, l2_cache_block[group][pos].data_32_low);
+            //dram_write(addr_old + 4, 4, l2_cache_block[group][pos].data_32_high);
         }
         l2_cache_block[group][pos].valid_bit = 1;
         l2_cache_block[group][pos].tag = tag;
