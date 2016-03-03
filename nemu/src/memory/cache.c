383660ed47b28c880e584b9e9755a44e649c2a19
tracer-ics2015 <tracer@njuics.org>
2016-03-03 20:13:28 +0800

    >  compile NEMU 14307130167 zyr17 Linux ubuntu 3.19.0-28-generic #30~14.04.1-Ubuntu SMP Tue Sep 1 09:32:55 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  20:13:28 up 1 day, 16:44,  2 users,  load average: 0.34, 1.24, 1.31 381d32fee18fe38bac9c2badcf54f62eb21c1b27

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -36,11 +36,11 @@ uint32_t L2_cache_single(hwaddr_t addr, size_t len){
             int ii;
             unsigned long long lltmp = 0;
             for (ii = 0; ii < BLOCK_SIZE / 8; ii ++ ){
-                lltmp = (lltmp << 8) + l2_cache_block[group][pos].data[ii];
+                lltmp = (lltmp << 8LL) + l2_cache_block[group][pos].data[ii];
                 //dram_write(addr_old + ii, 1, l2_cache_block[group][pos].data[ii]);
             }
             dram_write(addr_old, 4, lltmp & 0xffffffff);
-            dram_write(addr_old + 4, 4, lltmp >> 32);
+            dram_write(addr_old + 4, 4, lltmp >> 32LL);
         }
         l2_cache_block[group][pos].valid_bit = 1;
         l2_cache_block[group][pos].tag = tag;
