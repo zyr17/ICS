768167cf4daf455f647881f780562f7bce7b95f4
tracer-ics2015 <tracer@njuics.org>
2016-03-03 18:34:12 +0800

    >  compile NEMU 14307130167 zyr17 Linux ubuntu 3.19.0-28-generic #30~14.04.1-Ubuntu SMP Tue Sep 1 09:32:55 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  18:34:12 up 1 day, 15:05,  2 users,  load average: 0.85, 0.53, 0.33 24eb6aa94b1ded5074ff2b7da97500e7d5aa5fe

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -11,61 +11,6 @@ struct{
     uint8_t data[BLOCK_SIZE / 8];
 }l2_cache_block[L2_SET][L2_LENGTH];
 
-uint32_t L1_cache_single(hwaddr_t addr, size_t len){
-    int group = addr / (BLOCK_SIZE / 8) % L1_SET;
-    int tag = addr / (BLOCK_SIZE / 8) / L1_SET;
-    int start = addr % (BLOCK_SIZE / 8);
-    int pos = - 1;
-    int i, j;
-    for (i = 0; i < L1_LENGTH; i ++ )
-        if (l1_cache_block[group][i].valid_bit == 1 && tag == l1_cache_block[group][i].tag)
-            pos = i;
-    if (!~pos){
-        pos = rand() % L1_LENGTH;
-        l1_cache_block[group][pos].valid_bit = 1;
-        l1_cache_block[group][pos].tag = tag;
-        for (i = 0; i < BLOCK_SIZE / 8; i ++ )
-            l1_cache_block[group][pos].data[i] = dram_read(addr / (BLOCK_SIZE / 8) * (BLOCK_SIZE / 8) + i, 1);
-    }
-    uint32_t ans = 0;
-    for (j = len - 1; j >= 0; j -- )
-        ans = (ans << 8) + l1_cache_block[group][pos].data[j + start];
-    return ans;
-}
-
-uint32_t L1_cache_read(hwaddr_t addr, size_t len){
-    if (addr / (BLOCK_SIZE / 8) != (addr + len - 1) / (BLOCK_SIZE / 8)){
-        int tmp = (addr + len - 1) % (BLOCK_SIZE / 8) + 1;
-        uint32_t t1 = L1_cache_single(addr + (len - tmp), tmp);
-        t1 <<= ((len - tmp) * 8);
-        t1 += L1_cache_single(addr, len - tmp);
-        return t1;
-    }
-    else return L1_cache_single(addr, len);
-}
-
-void L1_cache_update(hwaddr_t addr, size_t len){
-    int group = addr / (BLOCK_SIZE / 8) % L1_SET;
-    int tag = addr / (BLOCK_SIZE / 8) / L1_SET;
-    int start = addr % (BLOCK_SIZE / 8);
-    int i, ii;
-    for (i = 0; i < L1_LENGTH; i ++ )
-        if (l1_cache_block[group][i].valid_bit == 1 && tag == l1_cache_block[group][i].tag){
-            for (ii = 0; ii < len; ii ++ )
-                l1_cache_block[group][i].data[ii + start] = dram_read(addr + ii, 1);
-        }
-}
-
-void L1_cache_write(hwaddr_t addr, size_t len, uint32_t data){
-    dram_write(addr, len, data);
-    if (addr / (BLOCK_SIZE / 8) != (addr + len - 1) / (BLOCK_SIZE / 8)){
-        int tmp = (addr + len - 1) % (BLOCK_SIZE / 8) + 1;
-        L1_cache_update(addr + (len - tmp), tmp);
-        L1_cache_update(addr, len - tmp);
-    }
-    else L1_cache_update(addr, len);
-}
-
 uint32_t L2_cache_single(hwaddr_t addr, size_t len){
     int group = addr / (BLOCK_SIZE / 8) % L2_SET;
     int tag = addr / (BLOCK_SIZE / 8) / L2_SET;
@@ -143,4 +88,59 @@ void L2_cache_write(hwaddr_t addr, size_t len, uint32_t data){
         L2_cache_update(addr, len - tmp, data >> (tmp * 8));
     }
     else L2_cache_update(addr, len, data);
+}
+
+uint32_t L1_cache_single(hwaddr_t addr, size_t len){
+    int group = addr / (BLOCK_SIZE / 8) % L1_SET;
+    int tag = addr / (BLOCK_SIZE / 8) / L1_SET;
+    int start = addr % (BLOCK_SIZE / 8);
+    int pos = - 1;
+    int i, j;
+    for (i = 0; i < L1_LENGTH; i ++ )
+        if (l1_cache_block[group][i].valid_bit == 1 && tag == l1_cache_block[group][i].tag)
+            pos = i;
+    if (!~pos){
+        pos = rand() % L1_LENGTH;
+        l1_cache_block[group][pos].valid_bit = 1;
+        l1_cache_block[group][pos].tag = tag;
+        for (i = 0; i < BLOCK_SIZE / 8; i ++ )
+            l1_cache_block[group][pos].data[i] = L2_cache_read(addr / (BLOCK_SIZE / 8) * (BLOCK_SIZE / 8) + i, 1);
+    }
+    uint32_t ans = 0;
+    for (j = len - 1; j >= 0; j -- )
+        ans = (ans << 8) + l1_cache_block[group][pos].data[j + start];
+    return ans;
+}
+
+uint32_t L1_cache_read(hwaddr_t addr, size_t len){
+    if (addr / (BLOCK_SIZE / 8) != (addr + len - 1) / (BLOCK_SIZE / 8)){
+        int tmp = (addr + len - 1) % (BLOCK_SIZE / 8) + 1;
+        uint32_t t1 = L1_cache_single(addr + (len - tmp), tmp);
+        t1 <<= ((len - tmp) * 8);
+        t1 += L1_cache_single(addr, len - tmp);
+        return t1;
+    }
+    else return L1_cache_single(addr, len);
+}
+
+void L1_cache_update(hwaddr_t addr, size_t len){
+    int group = addr / (BLOCK_SIZE / 8) % L1_SET;
+    int tag = addr / (BLOCK_SIZE / 8) / L1_SET;
+    int start = addr % (BLOCK_SIZE / 8);
+    int i, ii;
+    for (i = 0; i < L1_LENGTH; i ++ )
+        if (l1_cache_block[group][i].valid_bit == 1 && tag == l1_cache_block[group][i].tag){
+            for (ii = 0; ii < len; ii ++ )
+                l1_cache_block[group][i].data[ii + start] = dram_read(addr + ii, 1);
+        }
+}
+
+void L1_cache_write(hwaddr_t addr, size_t len, uint32_t data){
+    L2_cache_write(addr, len, data);
+    if (addr / (BLOCK_SIZE / 8) != (addr + len - 1) / (BLOCK_SIZE / 8)){
+        int tmp = (addr + len - 1) % (BLOCK_SIZE / 8) + 1;
+        L1_cache_update(addr + (len - tmp), tmp);
+        L1_cache_update(addr, len - tmp);
+    }
+    else L1_cache_update(addr, len);
 }
