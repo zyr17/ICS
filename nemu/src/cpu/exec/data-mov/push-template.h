acda66ec0f1ecd6c10d425679520c1bded835b5a
tracer-ics2015 <tracer@njuics.org>
2016-03-19 18:43:30 +0800

    >  compile NEMU 14307130167 zyr17 Linux ubuntu 3.19.0-28-generic #30~14.04.1-Ubuntu SMP Tue Sep 1 09:32:55 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  18:43:30 up 14:16,  2 users,  load average: 0.11, 0.12, 0.08 fe93639e6add11e25dd8a40529d2998e48a80e98

--- nemu/src/cpu/exec/data-mov/push-template.h
+++ nemu/src/cpu/exec/data-mov/push-template.h
@@ -3,8 +3,9 @@
 #define instr push
 
 static void do_execute () {
-
+    if (DATA_BYTE == 1) printf("1 byte push: %#x\n", cpu.esp);
     push_push(DATA_BYTE, op_src -> val);
+    if (DATA_BYTE == 1) printf("1 byte push end: %#x\n", cpu.esp);
 
 	print_asm_template1();
 }
