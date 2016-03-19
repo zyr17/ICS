eb3c77ab8f60fbfe5cff596bca6ef0c7fee713a0
tracer-ics2015 <tracer@njuics.org>
2016-03-19 16:40:07 +0800

    >  compile NEMU 14307130167 zyr17 Linux ubuntu 3.19.0-28-generic #30~14.04.1-Ubuntu SMP Tue Sep 1 09:32:55 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  16:40:07 up 12:13,  2 users,  load average: 0.39, 0.24, 0.18 4d07622da8ff5166a0f315924ecbcca6433b091

--- nemu/src/cpu/exec/misc/int-template.h
+++ nemu/src/cpu/exec/misc/int-template.h
@@ -5,7 +5,7 @@
 #if DATA_BYTE == 2 || DATA_BYTE == 4
 
 void raise_intr(uint8_t);
-make_helper(concat(int_i_, SUFFIX)){printf("1111111");
+make_helper(concat(int_i_, SUFFIX)){
     assert(DATA_BYTE == 4);
     uint8_t tmp = instr_fetch(eip + 1, 1);
     raise_intr(tmp);
