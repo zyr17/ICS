de0489e7952534b34d916bb1f98e2d2f00afc0b3
tracer-ics2015 <tracer@njuics.org>
2016-06-04 07:49:07 -0700

    >  compile kernel 14307130167 zyr17 Linux ubuntu 4.2.0-36-generic #42~14.04.1-Ubuntu SMP Fri May 13 17:27:22 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  07:49:07 up  3:07,  2 users,  load average: 0.24, 0.17, 0.14 4666895bbb4fe5674159fee31dae8a7cb69c7091

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -47,7 +47,11 @@ uint32_t loader() {
 			/* TODO: read the content of the segment from the ELF file
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
+#ifdef HAS_DEVICE
+            ide_read((void*)pa_to_va(nowaddr), (uint32_t)(ph -> p_offset), ph -> p_filesz);
+#else
 			ramdisk_read((void*)pa_to_va(nowaddr), (uint32_t)(ph -> p_offset), ph -> p_filesz);
+#endif
 
 
 			/* TODO: zero the memory region
