8a135559af17b54829b932fa982908ea512fa65a
tracer-ics2015 <tracer@njuics.org>
2016-06-04 21:10:08 -0700

    >  compile kernel 14307130167 zyr17 Linux ubuntu 4.2.0-36-generic #42~14.04.1-Ubuntu SMP Fri May 13 17:27:22 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  21:10:08 up 16:28,  3 users,  load average: 0.30, 0.42, 0.40 659936a07534363e7f9b8cd0167493ade4624a90

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -43,6 +43,7 @@ uint32_t loader() {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
             uint32_t nowaddr = mm_malloc(ph -> p_vaddr, ph -> p_memsz);
+            set_bp();
 
 			/* TODO: read the content of the segment from the ELF file
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
