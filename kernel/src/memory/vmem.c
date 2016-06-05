c40105a8ce2e5e505b6d9b1fa785cee0475771c8
tracer-ics2015 <tracer@njuics.org>
2016-06-04 18:58:14 -0700

    >  compile kernel 14307130167 zyr17 Linux ubuntu 4.2.0-36-generic #42~14.04.1-Ubuntu SMP Fri May 13 17:27:22 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  18:58:14 up 14:16,  2 users,  load average: 0.31, 0.26, 0.22 702f8b7cbcd5923d88e969505800dc4d508d0b9e

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -17,7 +17,7 @@ void create_video_mapping() {
 	 * some page tables to create this mapping.
 	 */
     PDE* u_pde = get_updir();
-    assert(!u_pde->present);
+    assert(!(u_pde->present));
     u_pde->present = 1;
     u_pde->page_frame = ((int)Vmem) >> 12;
     int i;
