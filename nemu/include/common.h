c1208de2dcd9f9bb0a8dad63ef01ec29645af469
tracer-ics2015 <tracer@njuics.org>
2016-06-05 00:35:22 -0700

    >  compile NEMU 14307130167 zyr17 Linux ubuntu 4.2.0-36-generic #42~14.04.1-Ubuntu SMP Fri May 13 17:27:22 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  00:35:22 up 19:54,  4 users,  load average: 0.52, 0.50, 0.37 c92a3399308546f37f8358396d848a43847c4b5c

--- nemu/include/common.h
+++ nemu/include/common.h
@@ -6,9 +6,9 @@
 /* You will define this macro in PA4 */
 #define HAS_DEVICE
 
-#define DEBUG
+//#define DEBUG
 //#define USE_CACHE
-#define LOG_FILE
+//#define LOG_FILE
 
 #include "debug.h"
 #include "macro.h"
