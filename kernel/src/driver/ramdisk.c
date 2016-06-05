d0d51a6b3d226e9b520195dd9e6d53d5a6e2289f
tracer-ics2015 <tracer@njuics.org>
2016-06-04 22:12:22 -0700

    >  compile kernel 14307130167 zyr17 Linux ubuntu 4.2.0-36-generic #42~14.04.1-Ubuntu SMP Fri May 13 17:27:22 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  22:12:22 up 17:31,  3 users,  load average: 0.24, 0.42, 0.35 6dd1aa3105beba4535a4d876b86ced22f4340d

--- kernel/src/driver/ramdisk.c
+++ kernel/src/driver/ramdisk.c
@@ -11,12 +11,14 @@
 
 /* read `len` bytes starting from `offset` of ramdisk into `buf` */
 void ramdisk_read(uint8_t *buf, uint32_t offset, uint32_t len) {
+			Log("use ramdisk");set_bp();
 	nemu_assert(offset + len < RAMDISK_SIZE);
 	memcpy(buf, RAMDISK_START + offset, len);
 }
 
 /* write `len` bytes starting from `buf` into the `offset` of ramdisk */
 void ramdisk_write(uint8_t *buf, uint32_t offset, uint32_t len) {
+			Log("use ramdisk");set_bp();
 	nemu_assert(offset + len < RAMDISK_SIZE);
 	memcpy(RAMDISK_START + offset, buf, len);
 }
