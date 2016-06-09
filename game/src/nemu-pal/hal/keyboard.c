798c8b951b60192bb138a814e692318f9597cea1
tracer-ics2015 <tracer@njuics.org>
2016-06-09 04:43:41 -0700

    >  compile game 14307130167 zyr17 Linux ubuntu 4.2.0-36-generic #42~14.04.1-Ubuntu SMP Fri May 13 17:27:22 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  04:43:41 up  3:42,  3 users,  load average: 1.37, 0.96, 0.67 817f14e22a9a1e9a1d5678f5620b40a555b7b905

--- game/src/nemu-pal/hal/keyboard.c
+++ game/src/nemu-pal/hal/keyboard.c
@@ -69,7 +69,7 @@ process_keys(void (*key_press_callback)(int), void (*key_release_callback)(int))
     for (; i < NR_KEYS; i ++ )
         if (key_state[i] ^ key_pressed[i]){
             re = 1;
-            Log("key_changed: 0x%x", keycode_array[i]);
+            Log("key_changed: 0x%x %d", keycode_array[i], key_state[i]);
             if ((key_pressed[i] = key_state[i]))
                 key_press_callback(keycode_array[i]);
             else key_release_callback(keycode_array[i]);
